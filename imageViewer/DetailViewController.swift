9ed2e413f8adc076c0eaddb281d2226949ccc002
azie-ginanjar <xstepnort@gmail.com>
2018-06-24 05:09:58 +0700

    add share functionality

--- imageViewer/DetailViewController.swift
+++ imageViewer/DetailViewController.swift
@@ -17,6 +17,9 @@ class DetailViewController: UIViewController {
 
         // Do any additional setup after loading the view.
         title = selectedImage
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
+        
         if let imageToLoad = selectedImage {
             imageView.image  = UIImage(named: imageToLoad)
         }
@@ -32,6 +35,12 @@ class DetailViewController: UIViewController {
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         navigationController?.hidesBarsOnTap = false
+    }
+    
+    @objc func shareTapped() {
+        let vc = UIActivityViewController(activityItems: [imageView.image!], applicationActivities: [])
+        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(vc, animated: true)
     }
 
     override func didReceiveMemoryWarning() {
